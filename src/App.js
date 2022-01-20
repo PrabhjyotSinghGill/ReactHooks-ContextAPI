16cea734a48feef15b5559a505c7e1f4338ac33d
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-20 15:10:15 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import {Header} from "./components/Header.jsx";
 import {Balance} from "./components/Balance.jsx";
+import {IncomeExpenses} from "./components/IncomeExpenses.jsx";
+import {TransactionList} from "./components/TransactionList.jsx";
+import {AddTransaction} from "./components/AddTransaction.jsx";
 import './App.css';
 
 function App() {
@@ -8,6 +12,9 @@ function App() {
       <Header></Header>
       <div className="container">
         <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
       </div>
     </div>
   );
