b87d3fae35e56cd8855306ae5c39b1629012b7fa
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-20 14:42:04 -0500

    Balance.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {Header} from "./components/Header.jsx";
+import {Balance} from "./components/Balance.jsx";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      
+      <Header></Header>
+      <div className="container">
+        <Balance></Balance>
+      </div>
     </div>
   );
 }
